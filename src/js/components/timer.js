be968be3606e4bd13bc5b250476ff4d49de1bd60
Sebastian Proost <sebastian.proost@gmail.com>
2016-08-28 16:03:58 +0200

    increased accurac

--- src/js/components/timer.js
+++ src/js/components/timer.js
@@ -2,38 +2,45 @@ import React from "react";
 
 import dateFns from 'date-fns';
 
+
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {running: false};
+
+    this.state = {running: false, last_time: new Date()};
   }
 
   _increase_time = () => {
     if (this.state.running) {
-      this.props.increase_time(100)
-      var t = setTimeout(this._increase_time, 100);
+      const current_time = new Date();
+      const interval = dateFns.differenceInMilliseconds(current_time, this.state.last_time);
+
+      this.props.increase_time(interval);
+
+      this.setState({...this.state, last_time: current_time}, () => {setTimeout(this._increase_time, 10)});
     }
   }
 
   _start = (ev) => {
     ev.preventDefault();
 
-    this.setState({running: true, start: new Date()}, () => {this._increase_time()});
+    this.setState({...this.state, running: true, last_time: new Date()}, () => {this._increase_time()});
   }
 
   _stop = (ev) => {
     ev.preventDefault();
 
-    var stop = new Date();
+    const current_time = new Date();
+    const interval = dateFns.differenceInMilliseconds(current_time, this.state.last_time);
 
-    this.setState({running: false});
-    this.props.add_time(this.props.current_time);
-    console.log(dateFns.differenceInMilliseconds(stop, this.state.start));
+    this.props.increase_time(interval);
+
+    this.setState({...this.state, running: false}, () => {this.props.add_time(this.props.current_time)});
   }
 
   _reset = (ev) => {
     ev.preventDefault();
-    this.setState({running: false}, () => {this.props.reset_time()});
+    this.setState({...this.state, running: false}, () => {this.props.reset_time()});
   }
 
   render() {
