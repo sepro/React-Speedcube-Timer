c7f58e9238ee4932ce88a9944adcd128600518f7
Sebastian Proost <sebastian.proost@gmail.com>
2016-08-28 18:20:59 +0200

    style for results

--- src/js/components/results/results.js
+++ src/js/components/results/results.js
@@ -18,11 +18,25 @@ class Results extends React.Component {
 
   }
 
+  _title = (n) => {
+    if (n === 0) {
+        return "No results yet"
+    } else if ( n === 1) {
+        return "Best result"
+    } else if (n < 5) {
+        return "Best " + n + " results"
+    } else {
+        return "Best 5 results"
+    }
+  }
+
   render() {
+    const results = this.props.results;
     return (
-      <div>
-          { this.props.results.sort(compareNumbers).slice(0,5).map((result,i ) => {
-              return <p>{ this._format_time(result) }</p>;
+      <div className="results">
+      <strong>{ this._title(results.length) }</strong>
+          { results.sort(compareNumbers).slice(0,5).map((result,i ) => {
+              return <p>{ this._format_time(result) } </p>;
           })}
       </div>
     );
